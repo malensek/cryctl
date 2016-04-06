431317f0cccc2a650733ac010afbce44f21ad8e8
Matthew Malensek <matt@malensek.net>
2016-04-05 23:48:03 -0600

    Update max Command length

--- src/Command.h
+++ src/Command.h
@@ -3,10 +3,12 @@
 
 #include <cstdint>
 
+#define MAX_COMMAND_LEN 22
+
 struct Command {
     uint8_t type;
     uint8_t length;
-    uint8_t data[24];
+    uint8_t data[MAX_COMMAND_LEN];
     uint16_t crc;
 };
 
